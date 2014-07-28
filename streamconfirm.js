5467566f641874ef46196763bd924dde51a9cc34
Luke Madhanga <lmadhanga@gmail.com>
2014-07-28 15:01:41 +0100

    z-index fix

--- streamconfirm.js
+++ streamconfirm.js
@@ -58,7 +58,7 @@ function stream_confirm(message, callback, moreinfo, opts) {
         if (!$('style[id="strcon"]').length) {
             $('head').append(
 '<style id="strcon">' + 
-'#streamconfirm {position: fixed;top: 0;left:0;width: 100%;height: 100%;font-family: inherit;}' +
+'#streamconfirm {position: fixed;top: 0;left:0;width: 100%;height: 100%;font-family: inherit;z-index:3000;}' +
 '#streamconfirmover {top: 0;left: 0;width: 100%;height: 100%;z-index: 1000;position: fixed;}'+
 '#streamconfirmmain {top: 50%;background: #FFF;max-width: 500px;width: 80%;z-index: 1001;padding: 20px 20px 10px;' +
 'box-sizing: border-box;box-shadow: 0px 0px 1px #000;position: relative;margin: -60px auto;}' +
